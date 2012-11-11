7d802f07384b49f0f7d7bf5a789974281365bd14
Christopher Hopkins <hopkins.chris@gmail.com>
2012-11-11 17:49:05 -0500

    Cover case when user request invite but doesn't leave their email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,24 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
+  def day_of_the_week
+    time = Time.now.strftime("%w")
+    case time
+      when time = '0'
+        content_tag :span, "Sunday"
+      when time = '1'
+        content_tag :span, "Monday"
+      when time = '2'
+        content_tag :span, "Tuesday"
+      when time = '3'
+        content_tag :span, "Wednesday"
+      when time = '4'
+        content_tag :span, "Thursday"
+      when time = '5'
+        content_tag :span, "Friday"
+      when time = '6'
+        content_tag :span, "Saturday"
+    end
+  end
+
 end
