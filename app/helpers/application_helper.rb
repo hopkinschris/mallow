6fe16c4bb0d808a7db7ebc6c4a952d38226dc808
Christopher Hopkins <hopkins.chris@gmail.com>
2012-11-30 10:20:02 -0500

    Remove name of unfollower, not useful. KISS.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,16 +14,16 @@ module ApplicationHelper
       style = options[:style]
     end
     src = Twitter.user(id).profile_image_url(size)
-    name = Twitter.user(id).name
     handle = '@' + Twitter.user(id).screen_name
     user_followers = Twitter.follower_ids(@user.uid.to_i).collection
     unfollower_status =
       if user_followers.include?(id)
-        content_tag(:span, "Following", :class => 'status', :style => "font-size:8px;color:#777;padding:2px 5px;margin-top:1px;text-transform:uppercase;background-color:#EEE;border-radius:3px;")
+        content_tag(:span, "Following", :class => 'status', :style => "font-size:10px;-webkit-font-smoothing:antialiased;color:#fff;padding:2px 5px;margin:0.3em 0;text-transform:uppercase;background-color:#6cd4ff;border-radius:3px;")
+      else
+        content_tag(:span, "Not Following", :class => 'status', :style => "font-size:10px;-webkit-font-smoothing:antialiased;color:#777;padding:2px 5px;margin:0.3em 0;text-transform:uppercase;background-color:#EEE;border-radius:3px;")
       end
     details = 
-      content_tag(:li, name, :class => 'name', :style => "font-size:12px;list-style:none;font-weight:500;color:#444;") +
-      content_tag(:li, handle, :class => 'handle', :style => "font-size:12px;list-style:none;color:#999;font-weight:300;margin-bottom:4px;") +
+      content_tag(:li, handle, :class => 'handle', :style => "font-size:20px;-webkit-font-smoothing:antialiased;list-style:none;font-weight:300;line-height:1.4em;color:#444;margin-bottom:4px;") +
       unfollower_status
     content_tag(:img, nil, :src => src, :class => 'avatar', :style => style) +
     content_tag(:ul, details, :style => "padding:0;margin:0 2.5em 0 0;vertical-align:top;display:inline-block;")
@@ -40,15 +40,15 @@ module ApplicationHelper
   # :bigger - 73px by 73px
   def unfollower_details(size, id)
     src = Twitter.user(id).profile_image_url(size)
-    name = Twitter.user(id).name
     handle = '@' + Twitter.user(id).screen_name
     current_user_followers = Twitter.follower_ids(current_user.uid.to_i).collection
     unfollower_status =
       if current_user_followers.include?(id)
-        content_tag(:span, "Following", :class => 'status')
+        content_tag(:span, "Following", :class => 'status following')
+      else
+        content_tag(:span, "Not Following", :class => 'status')
       end
     details =
-      content_tag(:span, name, :class => 'name') +
       content_tag(:span, handle, :class => 'handle') +
       unfollower_status
     content_tag(:img, nil, :src => src, :class => 'avatar') +
