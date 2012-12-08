bd315cc93fc049a320f1f604b009a318c3fa9fca
Christopher Hopkins <hopkins.chris@gmail.com>
2012-12-07 20:29:00 -0500

    Validate email field for feedback with REGEX at model level

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -9,4 +9,7 @@ class Feedback
                   :note
 
   validates_presence_of :email, :note
+
+  validates_format_of :email,
+                      :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
